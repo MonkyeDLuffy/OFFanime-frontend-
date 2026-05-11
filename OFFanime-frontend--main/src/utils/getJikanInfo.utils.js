ee2de608b1f99280ffaf6a15c046b6a7fdba0371
Wzz.Earth <animetoonsekai@gmail.com>
2026-05-11 17:32:21 +0530

    Refactor getJikanInfo to use anilistId and fetch MAL ID

--- OFFanime-frontend--main/src/utils/getJikanInfo.utils.js
+++ OFFanime-frontend--main/src/utils/getJikanInfo.utils.js
@@ -1,16 +1,24 @@
+import getMalId from "./getMalId.utils";
+
 const API =
   "https://anime-details-api.onrender.com/api/jikan/anime";
 
-export default async function getJikanInfo(id) {
+export default async function getJikanInfo(anilistId) {
   try {
-    const response = await fetch(`${API}/${id}`);
+    // convert anilist -> MAL
+    const malId = await getMalId(anilistId);
+
+    if (!malId) {
+      console.log("No MAL ID found");
+
+      return null;
+    }
+
+    const response = await fetch(`${API}/${malId}`);
 
     const json = await response.json();
 
-    if (!json?.data) {
-      console.log("No Jikan data found");
-      return null;
-    }
+    if (!json?.data) return null;
 
     return json.data;
   } catch (err) {
