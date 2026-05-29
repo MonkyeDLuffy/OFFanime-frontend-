1b8f0f7bdc6a35683d4158340f709b17dcb0f0d2
Wzz.Earth <animetoonsekai@gmail.com>
2026-05-29 19:53:33 +0530

    Update API URL and fetch endpoint in getJikanInfo

--- OFFanime-frontend--main/src/utils/getJikanInfo.utils.js
+++ OFFanime-frontend--main/src/utils/getJikanInfo.utils.js
@@ -1,15 +1,14 @@
-const API = "https://anime-details-api.onrender.com/api/jikan/from-anilist";
+const API = import.meta.env.VITE_API_URL;
 
 export default async function getJikanInfo(anilistId) {
   try {
-    const response = await fetch(`${API}/${anilistId}`);
+    const response = await fetch(`${API}/jikan/anime/${anilistId}`);
 
     if (!response.ok) {
       throw new Error(`Jikan API failed: ${response.status}`);
     }
 
     const json = await response.json();
-
     return json?.data || null;
   } catch (err) {
     console.log("Jikan fetch failed:", err.message);
